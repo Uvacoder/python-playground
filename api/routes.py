06ee538c95fd23b9f255d5741c1f3513cbf93964
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-08-12 09:28:29 +0000

    ⚡️ avoid saving code under another id

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,6 @@
 from config import settings
 from deta import Deta
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter
 
 from .schema import CodePayload
 from .utils import execute_code as execute
@@ -28,4 +28,7 @@ async def execute_code(payload: CodePayload):
 @api.post("/save")
 async def save_code_snippet(payload: CodePayload):
     code = payload.code
+    existing = db.fetch(query={"code": code}).items
+    if len(existing) != 0:
+        return existing[0]
     return db.put(data={"code": code})
