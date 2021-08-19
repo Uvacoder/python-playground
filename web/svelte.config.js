da95d5a0760d03d1a67e1ecd52b2cdc0ab774471
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-08-19 18:12:26 +0530

    🔥 setup svelte-kit static for frontend

---
+++ web/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from "@sveltejs/adapter-static"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		ssr: false,
+	}
+};
+
+export default config;
